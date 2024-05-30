4e58034c8d1800ad89fd27e07611d56140057b86
zyairelai <zyairelai@gmail.com>
2024-05-30 11:31:56 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,46 +1,27 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.storage.local.get('proxyEnabled', (data) => {
-    const proxyEnabled = !data.proxyEnabled;
-    chrome.storage.local.set({ proxyEnabled: proxyEnabled }, () => {
-      if (proxyEnabled) {
-        chrome.proxy.settings.set(
-          {
-            value: {
-              mode: 'fixed_servers',
-              rules: {
-                singleProxy: {
-                  scheme: 'http',
-                  host: '127.0.0.1',
-                  port: 8080
-                },
-                bypassList: [
-                  'www.google.com',
-                  'www.googleapis.com',
-                  'www.gstatic.com',
-                  'clients4.google.com',
-                  'content-autofill.googleapis.com',
-                  'lh3.googleusercontent.com',
-                  'ogs.google.com',
-                  'optimizationguide-pa.googleapis.com',
-                  'play.google.com',
-                  'challenges.cloudflare.com',
-                  'connect.facebook.net',
-                  'safebrowsing.google.com',
-                  'syndication.twitter.com'
-                ]
-              }
-            },
-            scope: 'regular'
+  chrome.proxy.settings.set(
+    {
+      value: {
+        mode: 'fixed_servers',
+        rules: {
+          singleProxy: {
+            scheme: 'http',
+            host: '127.0.0.1',
+            port: 8080
           },
-          () => {
-            chrome.tabs.reload(tab.id);
-          }
-        );
-      } else {
+          bypassList: []
+        }
+      },
+      scope: 'regular'
+    },
+    () => {
+      chrome.tabs.reload(tab.id, { bypassCache: true }); // Reload the current tab
+      setTimeout(() => {
         chrome.proxy.settings.clear({ scope: 'regular' }, () => {
-          chrome.tabs.reload(tab.id);
+          // Clear the proxy settings after a delay (adjust the delay as needed)
+          console.log("Proxy settings cleared.");
         });
-      }
-    });
-  });
+      }, 3000); // Adjust the delay as needed (in milliseconds)
+    }
+  );
 });
